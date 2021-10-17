d89e87d2e73e545030125f9b642625f6c5dc4298
akki1027 <akki10271995@gmail.com>
2021-10-17 14:27:57 +0900

    バンドルのテスト実行

--- src/js/index.js
+++ src/js/index.js
@@ -1,55 +1,9 @@
 'use strict';
 
+import { Calculator } from "./calculator.js";
+
 
 {
-    /**
-     * @class
-     * @description - 計算クラス
-     */
-    class Calculator {
-
-        /**
-         *
-         * @param {Number} x - 数字1
-         * @param {Number} y - 数字2
-         * @returns {Number} - xとyを足した数字
-         */
-        sum(x, y) {
-            return x + y;
-        }
-
-        /**
-         *
-         * @param {Number} x - 数字1
-         * @param {Number} y - 数字2
-         * @returns {Number} - xからyを引いた数字
-         */
-        subtract(x, y) {
-            return x - y;
-        }
-
-        /**
-         *
-         * @param {Number} x - 数字1
-         * @param {Number} y - 数字2
-         * @returns {Number} - xとyを掛けた数字
-         */
-        multiply(x, y) {
-            return x * y;
-        }
-
-        /**
-         *
-         * @param {Number} x - 数字1
-         * @param {Number} y - 数字2
-         * @returns {Number} - xからyを割った数字
-         */
-        divide(x, y) {
-            return x / y;
-        }
-    }
-
-
     const calc = new Calculator();
 
     console.log(calc.sum(10, 2));
